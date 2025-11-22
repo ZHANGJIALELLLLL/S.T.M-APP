95130dbc9c32a4d3c4949e99073a69436e7c59b7
ZHANG <annedaiislele@gmail.com>
2025-11-22 16:58:17 +0800

    Unit test login page

---
+++ app/src/androidTest/java/com/example/cp3406_stm_app/LoginScreenTest.kt
@@ -0,0 +1,49 @@
+package com.example.cp3406_stm_app
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.cp3406_stm_app.ui.LoginScreen
+import org.junit.Rule
+import org.junit.Test
+
+class LoginScreenTest {
+
+
+    @get:Rule
+//    val composeRule = createAndroidComposeRule<MainActivity>()
+    val composeRule = createComposeRule()
+
+
+
+    @Test
+    fun loginButton_disabled_whenEmptyEmail() {
+        composeRule.setContent {
+            val navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            LoginScreen(navController = navController)
+        }
+
+//        composeRule.onNode(hasSetTextAction()).performTextInput("")
+        composeRule.onNodeWithText("Continue").assertIsNotEnabled()
+    }
+
+    @Test
+    fun loginButton_enabled_whenEmailEntered() {
+        composeRule.setContent {
+            val navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            LoginScreen(navController = navController)
+        }
+
+        composeRule.onNodeWithText("Email").performTextInput("user@example.com")
+        composeRule.onNodeWithText("Continue").assertIsEnabled()
+    }
+}
